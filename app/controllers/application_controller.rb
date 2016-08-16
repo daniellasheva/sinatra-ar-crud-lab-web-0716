64740b9d10a81e5f7cb440558075732a592a37a4
daniellasheva <daniellasheva@gmail.com>
2016-08-16 17:50:37 -0400

    initial commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 require_relative '../../config/environment'
+require 'pry'
 
 class ApplicationController < Sinatra::Base
 
@@ -7,7 +8,56 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  get '/' do 
-    
+  # get '/' do
+  #
+  # end
+
+  get '/posts/new' do
+    erb :new
   end
+
+  post "/posts" do
+    @post= Post.create(params)
+    redirect "/posts"
+  end
+
+  get "/posts" do
+    if params[:message]
+      @message= params[:message]
+    end
+    @posts=Post.all
+    erb :index
+  end
+
+  get "/posts/:id" do
+    @post=Post.find(params[:id])
+    erb :show
+  end
+
+  get '/posts/:id/edit' do
+    #renders edit page
+    #binding.pry
+    @post= Post.find(params[:id])
+    erb :edit
+  end
+
+  patch '/posts/:id/edit' do
+    @post=Post.find(params[:id])
+    @post.update(name: params[:name], content: params[:content])
+    redirect "/posts/#{@post.id}"
+  end
+
+  # get '/posts/:id/delete' do
+  #   @post= Post.find(params[:id])
+  # end
+
+  delete '/posts/:id/delete' do
+    @post=Post.find(params[:id])
+    @message= "#{@post.name} was deleted"
+    @post.delete
+    redirect "/posts?message=#{@message}"
+  end
+
+  #redirect from post methods and use erb show for get methods
+
 end
